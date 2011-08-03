95c8fb7e1e8191697ee42bc883c31b3d952509e5
K2Da TSUDA <id.k2da@gmail.com>
2011-08-03 13:26:44 -0400

    cookie ng

--- up/public/script.js
+++ up/public/script.js
@@ -1,10 +1,41 @@
 function ng(id) {
-  $("." + id).animate({ opacity: "hide" }, "slow");
-  if($.cookie == null)
-    $.cookie('ng') = id
-  else
-    $.cookie('ng') = $.cookie('ng') + "," + id
+  hide = true;
+  ngid = $.cookie('ng')
+
+  if(ngid == null || ngid == '') {
+    hide = true;
+    $.cookie('ng', id, { path: '/' })
+  } else {
+    if(ngid.indexOf(id) == -1) {
+      hide = true;
+      $.cookie('ng', $.cookie('ng') + "," + id, { path: '/' })
+    } else {
+      hide = false;
+      ngid = ngid.split(',' + id).join('').split(id + ',').join('')
+      $.cookie('ng', ngid, { path: '/' })
+    }
+  }
+
+  res =  $("." + id);
+  if(hide) {
+    $(".r", res).hide();
+    $(".i .n", res).text("unNG");
+  } else {
+    $(".r", res).show();
+    $(".i .n", res).text("NG");
+  }
 }
 
-function dong() {
+function nr() {
+  ngid = $.cookie('ng')
+  if(ngid != null && ngid != '') {
+    selector = "." + $.cookie('ng').split(",").join(", .");
+    $(".r", selector).hide();
+    $(".i .n", selector).text("unNG");
+  }
+}
+
+function clear_ng(){
+  alert("clear " + $.cookie('ng'));
+  $.cookie('ng', '', { path: '/' });
 }
